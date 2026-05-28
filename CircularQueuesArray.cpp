0c03ed678aca80a3298c8d7cea70109701a6aa8b
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-28 18:34:12 +0700

    algorithm check if antrian kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -41,5 +41,12 @@ class Queues{
             queue_array[REAR] = num; 
         };
 
-        void remove(){};
+        void remove(){
+            //cek apakah antrian kosong
+            if(FRONT == -1){
+                cout << "Queue underflow\n";
+                return;
+            }
+            cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] <<"\n";
+        };
 };
